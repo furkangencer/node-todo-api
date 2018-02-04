1f0ce3721ac43a93273f259ca8beaccec0813bd6
Furkan <fgencer12@gmail.com>
2018-02-04 16:45:06 +0300

    User model

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+// Important! Mongoose buffers all the commands until it's connected to the database.
+// This means that you don't have to wait until it connects to MongoDB in order to define models, run queries, etc.
 
 // Mongoose let's you integrate your own Promise library if you like.
 // In order to use regular ES6 Promises with Mongoose though, you must explicitly specify global.Promise which is the built-in JavaScript Promise.
@@ -8,16 +10,22 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
+/*
 // Create instance of model
 var newTodo = new Todo({
     text: 'Cook dinner'
@@ -25,21 +33,27 @@ var newTodo = new Todo({
 
 // Save to db
 newTodo.save().then((doc) => {
-    console.log('Saved todo', doc);
+    console.log('Saved todo', JSON.stringify(doc,undefined,2));
 }, (e) => {
-    console.log('Unable to save todo');
+    console.log('Unable to save todo', e);
+});*/
+
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
 });
 
-/*
-var otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
+var user = new User({
+    email: 'petrucci@example.com'
 });
 
-otherTodo.save().then((doc) => {
-    console.log(JSON.stringify(doc,undefined,2));
+user.save().then((doc) => {
+    console.log('User saved', doc);
 }, (e) => {
-    console.log('Unable to save', e);
+    console.log('Unable to save user', e);
 });
-*/
