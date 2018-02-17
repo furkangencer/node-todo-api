2a768adae162b3df128a2eceafa507d88f5891cc
Furkan <fgencer12@gmail.com>
2018-02-17 16:14:13 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+var {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -31,6 +32,27 @@ app.get('/todos', (req, res) => {
             todos
         })
     }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        // Invalid ObjectID
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            // Document not found
+            return res.status(404).send();
+        }
+
+        // Document found
+        res.send({todo: todo});
+    }).catch((e) => {
         res.status(400).send(e);
     })
 });
