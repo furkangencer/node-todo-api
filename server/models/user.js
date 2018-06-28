1696f0f79d603cbd7e8cb2a172bdd106a3f9bc92
Furkan <fgencer12@gmail.com>
2018-06-28 22:56:01 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                }else{
+                    reject();
+                }
+            });
+        })
+    })
+};
+
 // The code to run before a given event (In this case, it's save() event)
 // We want to hash the user's password before saving it to database.
 UserSchema.pre('save', function (next) {
