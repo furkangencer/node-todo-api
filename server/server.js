a60aaa4c1aae851f572ed4867eb415bb57cd8309
Furkan <fgencer12@gmail.com>
2018-06-28 23:33:27 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -116,6 +116,11 @@ app.post('/users', (req, res) => {
     })
 });
 
+// This route will be using a middleware
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -128,10 +133,13 @@ app.post('/users/login', (req, res) => {
     });
 });
 
-// This route will be using a middleware
-app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
-});
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
